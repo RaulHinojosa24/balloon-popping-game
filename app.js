7a13e450077cbbf6ca75eac6764b61b05ccc5fe6
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-10 12:56:59 +0100

    Solucionado

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // tu código aquí
+
+const balloons = document.querySelectorAll(".balloon");
+let poppedBalloons = 0;
+
+balloons.forEach(element => {
+    element.addEventListener("mouseover", function (event) {
+        event.target.style.background = 0;
+        event.target.textContent = "POP!";
+        poppedBalloons++;
+
+        checkAllBalloonsPopped();
+    });
+});
+
+function checkAllBalloonsPopped() {
+    if (balloons.length == poppedBalloons) {
+        document.querySelector("#yay-no-balloons").style.display = "block";
+        document.querySelector("#balloon-gallery").style.display = "none";
+    }
+}
